721d222cb0c89c08f52de78046cc16430be064e1
PrashantRao63863 <prashant63863@gmail.com>
2021-06-20 01:13:09 +0530

    initial

--- src/components/home.js
+++ src/components/home.js
@@ -20,7 +20,7 @@ const HomeComponent = props => {
                             <li><a className="nav-link scrollto " href="#portfolio">Portfolio</a></li>
                             <li><a className="nav-link scrollto" href="#team">Team</a></li>
                             <li><a href="blog.html">Blog</a></li>
-                            <li className="dropdown"><a href="#"><span>Filmy Down</span> <i className="bi bi-chevron-down"></i></a>
+                            <li className="dropdown"><a href="#"><span>Filmy upkaran services</span> <i className="bi bi-chevron-down"></i></a>
                                 <ul>
                                     <li><a href="#">Drop Down 1</a></li>
                                     <li className="dropdown"><a href="#"><span>Deep Drop Down</span> <i className="bi bi-chevron-right"></i></a>
