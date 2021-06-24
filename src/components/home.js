79121a99d594e66b430a406c01021952f003d0cc
PrashantRao63863 <prashant63863@gmail.com>
2021-06-24 16:43:40 +0530

    ..

--- src/components/home.js
+++ src/components/home.js
@@ -309,55 +309,55 @@ const HomeComponent = props => {
                         <ul className="faq-list">
 
                             <li>
-                                <div data-bs-toggle="collapse" className="collapsed question" href="#faq1">Non consectetur a erat nam at lectus urna duis? <i className="bi bi-chevron-down icon-show"></i><i className="bi bi-chevron-up icon-close"></i></div>
+                                <div data-bs-toggle="collapse" className="collapsed question" href="#faq1">What is filmy Upkaran? <i className="bi bi-chevron-down icon-show"></i><i className="bi bi-chevron-up icon-close"></i></div>
                                 <div id="faq1" className="collapse" data-bs-parent=".faq-list">
                                     <p>
-                                        Feugiat pretium nibh ipsum consequat. Tempus iaculis urna id volutpat lacus laoreet non curabitur gravida. Venenatis lectus magna fringilla urna porttitor rhoncus dolor purus non.
+                                    Filmy upkaran is a website where we offer renting and buying equipments for making professional and entertaining videos. And also providing professional crew support for proper guidance at applicable rates.
                                     </p>
                                 </div>
                             </li>
 
                             <li>
-                                <div data-bs-toggle="collapse" href="#faq2" className="collapsed question">Feugiat scelerisque varius morbi enim nunc faucibus a pellentesque? <i className="bi bi-chevron-down icon-show"></i><i className="bi bi-chevron-up icon-close"></i></div>
+                                <div data-bs-toggle="collapse" href="#faq2" className="collapsed question">Is the equipments only available for rent service? <i className="bi bi-chevron-down icon-show"></i><i className="bi bi-chevron-up icon-close"></i></div>
                                 <div id="faq2" className="collapse" data-bs-parent=".faq-list">
                                     <p>
-                                        Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi. Id interdum velit laoreet id donec ultrices. Fringilla phasellus faucibus scelerisque eleifend donec pretium. Est pellentesque elit ullamcorper dignissim. Mauris ultrices eros in cursus turpis massa tincidunt dui.
+                                        No, buying and and renting both are available for everyone.
                                     </p>
                                 </div>
                             </li>
 
                             <li>
-                                <div data-bs-toggle="collapse" href="#faq3" className="collapsed question">Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi? <i className="bi bi-chevron-down icon-show"></i><i className="bi bi-chevron-up icon-close"></i></div>
+                                <div data-bs-toggle="collapse" href="#faq3" className="collapsed question">Is there any support service to help me if i face any sortproblem? <i className="bi bi-chevron-down icon-show"></i><i className="bi bi-chevron-up icon-close"></i></div>
                                 <div id="faq3" className="collapse" data-bs-parent=".faq-list">
                                     <p>
-                                        Eleifend mi in nulla posuere sollicitudin aliquam ultrices sagittis orci. Faucibus pulvinar elementum integer enim. Sem nulla pharetra diam sit amet nisl suscipit. Rutrum tellus pellentesque eu tincidunt. Lectus urna duis convallis convallis tellus. Urna molestie at elementum eu facilisis sed odio morbi quis
+                                       24*7 customer service is available to solve all your problems.
                                     </p>
                                 </div>
                             </li>
 
                             <li>
-                                <div data-bs-toggle="collapse" href="#faq4" className="collapsed question">Ac odio tempor orci dapibus. Aliquam eleifend mi in nulla? <i className="bi bi-chevron-down icon-show"></i><i className="bi bi-chevron-up icon-close"></i></div>
+                                <div data-bs-toggle="collapse" href="#faq4" className="collapsed question">Is filmy upkaran safe? <i className="bi bi-chevron-down icon-show"></i><i className="bi bi-chevron-up icon-close"></i></div>
                                 <div id="faq4" className="collapse" data-bs-parent=".faq-list">
                                     <p>
-                                        Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi. Id interdum velit laoreet id donec ultrices. Fringilla phasellus faucibus scelerisque eleifend donec pretium. Est pellentesque elit ullamcorper dignissim. Mauris ultrices eros in cursus turpis massa tincidunt dui.
+                                       Filmy upkaran follows all safety steps and keeps user data safe.
                                     </p>
                                 </div>
                             </li>
 
                             <li>
-                                <div data-bs-toggle="collapse" href="#faq5" className="collapsed question">Tempus quam pellentesque nec nam aliquam sem et tortor consequat? <i className="bi bi-chevron-down icon-show"></i><i className="bi bi-chevron-up icon-close"></i></div>
+                                <div data-bs-toggle="collapse" href="#faq5" className="collapsed question">When was filmy upkaran found? <i className="bi bi-chevron-down icon-show"></i><i className="bi bi-chevron-up icon-close"></i></div>
                                 <div id="faq5" className="collapse" data-bs-parent=".faq-list">
                                     <p>
-                                        Molestie a iaculis at erat pellentesque adipiscing commodo. Dignissim suspendisse in est ante in. Nunc vel risus commodo viverra maecenas accumsan. Sit amet nisl suscipit adipiscing bibendum est. Purus gravida quis blandit turpis cursus in
+                                       It was found on 20th June 2021.
                                     </p>
                                 </div>
                             </li>
 
                             <li>
-                                <div data-bs-toggle="collapse" href="#faq6" className="collapsed question">Tortor vitae purus faucibus ornare. Varius vel pharetra vel turpis nunc eget lorem dolor? <i className="bi bi-chevron-down icon-show"></i><i className="bi bi-chevron-up icon-close"></i></div>
+                                <div data-bs-toggle="collapse" href="#faq6" className="collapsed question">Who is the founder of filmy upkaran? <i className="bi bi-chevron-down icon-show"></i><i className="bi bi-chevron-up icon-close"></i></div>
                                 <div id="faq6" className="collapse" data-bs-parent=".faq-list">
                                     <p>
-                                        Laoreet sit amet cursus sit amet dictum sit amet justo. Mauris vitae ultricies leo integer malesuada nunc vel. Tincidunt eget nullam non nisi est sit amet. Turpis nunc eget lorem dolor sed. Ut venenatis tellus in metus vulputate eu scelerisque. Pellentesque diam volutpat commodo sed egestas egestas fringilla phasellus faucibus. Nibh tellus molestie nunc non blandit massa enim nec.
+                                        Filmy upkaran was developed by Prashant Rao and Prakhar Gupta under the guidence of Mohammad Mubassir Sir
                                     </p>
                                 </div>
                             </li>
